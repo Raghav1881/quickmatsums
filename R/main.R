fdce5ea2066e9b1b0770d5e571246c60407c9d08
Raghav Sharma <59401149+Raghav1881@users.noreply.github.com>
2023-10-11 09:50:58 -0400

    update exports

--- R/main.R
+++ R/main.R
@@ -1,14 +1,17 @@
+#' @import methods
 library(RcppArmadillo)
 
-#' @export
-# Add rows of sparse matrix and return a vector contaning the sum of all row-wise elements
+#' Add rows of sparse matrix and return a vector containing the sum of all row-wise elements
+#' @param mat A sparse matrix
+#' @export quickRowSums
 quickRowSums <- function(mat) {
   res <- add_rows_cpp(mat)
   return(res)
 }
 
-#' @export
-# Add columns of sparse matrix and return a vector contaning the sum of all column-wise elements
+#' Add columns of sparse matrix and return a vector containing the sum of all column-wise elements
+#' @param mat A sparse matrix
+#' @export quickColSums
 quickColSums <- function(mat) {
   res <- add_cols_cpp(mat)
   return(res)
